ecea4be6c1b73518de6efd3a0cbd7526eefa848d
Kevin Alexandre Riaño Vargas <31933448+karianov@users.noreply.github.com>
2022-11-02 21:30:43 -0500

    Enhance health controller response (#2) (#3)

--- src/main/java/co/edu/karianov/controller/HealthController.java
+++ src/main/java/co/edu/karianov/controller/HealthController.java
@@ -1,15 +1,21 @@
 package co.edu.karianov.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.karianov.controller.response.ServiceStatusResponse;
 
 @RestController(value = "/v1")
 public class HealthController {
 
 	@GetMapping("health")
-	public ResponseEntity<String> checkHealthStatus() {
-		return ResponseEntity.ok("Service is up");
+	public ResponseEntity<ServiceStatusResponse> checkHealthStatus() {
+		ServiceStatusResponse serviceStatusResponse = new ServiceStatusResponse();
+		serviceStatusResponse.setCode(HttpStatus.OK.value());
+		serviceStatusResponse.setStatus("Service is up");
+		return ResponseEntity.ok(serviceStatusResponse);
 	}
 
 }
---
+++ src/main/java/co/edu/karianov/controller/response/ServiceStatusResponse.java
@@ -0,0 +1,15 @@
+package co.edu.karianov.controller.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ServiceStatusResponse {
+
+	private Integer code;
+	private String status;
+
+}
