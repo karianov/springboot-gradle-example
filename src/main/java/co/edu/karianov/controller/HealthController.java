b91424ef92239a6504521aec7236b2855b0b2634
karianov <karianov@correo.udistrital.edu.co>
2022-11-02 22:23:17 -0500

    Set all controllers prefix path

--- src/main/java/co/edu/karianov/controller/HealthController.java
+++ src/main/java/co/edu/karianov/controller/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.karianov.controller.response.ServiceStatusResponse;
 
-@RestController(value = "/v1")
+@RestController
 public class HealthController {
 
 	@GetMapping("health")
--- src/main/java/co/edu/karianov/controller/response/ServiceStatusResponse.java
+++ src/main/java/co/edu/karianov/controller/response/ServiceStatusResponse.java
@@ -2,11 +2,9 @@ package co.edu.karianov.controller.response;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class ServiceStatusResponse {
 
 	private Integer code;
